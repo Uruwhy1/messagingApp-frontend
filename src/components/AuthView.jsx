4617c23f3667ed6d1a4ef183720c8203db0f0fbf
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-12-26 16:59:15 -0300

    finish styling auth view

--- src/components/AuthView.jsx
+++ src/components/AuthView.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import PropTypes from "prop-types";
 import styles from "./AuthView.module.css";
 import LoginMain from "../svgs/LoginMain";
 import Form from "./Form";
+import { X } from "lucide-react";
 
 const AuthForm = ({ mode, setView }) => {
   return (
     <div className={styles.container}>
+      <button onClick={() => setView(null)} className={styles.close}>
+        <X />
+      </button>
       <div className={styles.left}>
         <LoginMain />
       </div>
@@ -14,6 +18,11 @@ const AuthForm = ({ mode, setView }) => {
       </div>
     </div>
   );
+};
+
+AuthForm.propTypes = {
+  mode: PropTypes.string.isRequired,
+  setView: PropTypes.func.isRequired,
 };
 
 export default AuthForm;
