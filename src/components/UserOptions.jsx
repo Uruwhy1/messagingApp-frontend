bf62fd4cb6171b57e9ead825daaa443187656113
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2025-01-03 22:39:36 -0300

    add logout button

--- src/components/UserOptions.jsx
+++ src/components/UserOptions.jsx
@@ -1,13 +1,28 @@
 import styles from "./UserOptions.module.css";
 import { useWebSocket } from "../contexts/WebSocketContext";
-import UserPicture from "./UserPicture";
+import UserPicture from "./reusable/UserPicture";
+import { LogOut } from "lucide-react";
 
 const UserOptions = () => {
-  const { user } = useWebSocket();
+  const { user, fetchData } = useWebSocket();
 
   return (
     <div className={styles.userContainer}>
-      <UserPicture user={user} />
+      <div className={styles.userStuff}>
+        <UserPicture user={user} />
+
+        <div className={styles.menuContainer}>
+          <div className={styles.menu}>
+            <button
+              onClick={() => fetchData("/users/logout", "POST")}
+              className={styles.logoutButton}
+              title="Log Out"
+            >
+              <LogOut size={20} />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
